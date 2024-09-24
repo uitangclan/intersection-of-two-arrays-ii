6ffb91478b7a8b4e0a1df95ea62865632acb16de
uitangclan <uitangclan@gmail.com>
2024-09-24 10:47:12 +0900

    happy coding

---
+++ intersect.kt
@@ -0,0 +1,10 @@
+fun main (){
+    var nums1 = intArrayOf(1,2,2,1)
+    var nums2 = intArrayOf(2,2)
+    // var nums1 = intArrayOf(4,9,5)
+    // var nums2 = intArrayOf(9,4,9,8,4)
+    println(intersect(nums, k))
+}
+
+fun intersect(nums1: IntArray, nums2: IntArray): IntArray {
+}
