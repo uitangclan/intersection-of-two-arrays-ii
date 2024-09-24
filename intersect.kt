939064984552854aa33edde32f93a8bd7f58b9c7
uitangclan <uitangclan@gmail.com>
2024-09-25 06:12:30 +0900

    happy coding

--- intersect.kt
+++ intersect.kt
@@ -8,23 +8,21 @@ fun main (){
 
 fun intersect(nums1: IntArray, nums2: IntArray): IntArray {
 
-    var map = HashMap<Int, Int> ()
     nums1.sort()
     nums2.sort()
 
-    if (nums1.size > nums2.size) {
-        return intersect(nums2, nums1)
-    }
-
-    for(n in nums1) {
-        map.put(n, map.getOrDefault(n, 0) + 1)
-    }
+    var i = 0
+    var j = 0
     var k = 0
-    for(n in nums2) {
-        var cnt = map.getOrDefault(n, 0)
-        if (0 < cnt) {
-            nums1[k++] = n
-            map.put(n, cnt - 1)
+
+    while (i < nums1.size && j < nums2.size) {
+        if (nums1[i] < nums2[j]) {
+            i++
+        } else if (nums2[j] < nums1[i]) {
+            j++
+        } else {
+            nums1[k++] = nums1[i++]
+            j++
         }
     }
     return nums1.copyOfRange(0, k)
